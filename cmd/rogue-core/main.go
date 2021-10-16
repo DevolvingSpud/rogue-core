e786369dcf7070ed39f61cc9f96d3117b03835d6
DevolvingSpud <DevolvingSpud@gmail.com>
2021-10-16 11:17:39 -0400

    Upped to 1.17

--- cmd/rogue-core/main.go
+++ cmd/rogue-core/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"github.com/DevolvingSpud/rogue-core/internal/entities"
+	tl "github.com/JoelOtter/termloop"
+)
+
 func main() {
-	return
+	game := tl.NewGame()
+
+	mapLevel := tl.NewBaseLevel(tl.Cell{
+		Bg: tl.ColorGreen,
+		Fg: tl.ColorBlack,
+		Ch: '.',
+	})
+	mapLevel.AddEntity(tl.NewRectangle(10, 10, 50, 20, tl.ColorBlue))
+	game.Screen().SetLevel(mapLevel)
+
+	player := entities.Player{
+		Entity: tl.NewEntity(1, 1, 1, 1),
+		Level:  mapLevel,
+	}
+	player.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '@'})
+	mapLevel.AddEntity(&player)
+
+	game.Start()
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,14 @@
 module github.com/DevolvingSpud/rogue-core
 
-go 1.16
+go 1.17
 
-require github.com/magefile/mage v1.11.0
+require (
+	github.com/JoelOtter/termloop v0.0.0-20201118115657-7fa23b4da654
+	github.com/magefile/mage v1.11.0
+)
+
+require (
+	github.com/mattn/go-runewidth v0.0.10 // indirect
+	github.com/nsf/termbox-go v1.1.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+)
